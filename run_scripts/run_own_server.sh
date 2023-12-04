c643d29670a9dfa3772dba56c5c56886fa9daaea
chouheiwa <849131492@qq.com>
2023-12-04 19:44:00 +0800

    Update yaml and script.

--- run_scripts/run_own_server.sh
+++ run_scripts/run_own_server.sh
@@ -1,11 +1,11 @@
-CONFIG_PATH='config/own_server/'
+CONFIG_PATH='config/full-config/own_server/'
 
 # U_Net/R2U_Net/AttU_Net/R2AttU_Net
 
-python main.py --yaml_path=$CONFIG_PATH'U_Net.yaml'
+python main.py --yaml_path=$CONFIG_PATH'UNet.yaml'
 
-python main.py --yaml_path=$CONFIG_PATH'R2U_Net.yaml'
+python main.py --yaml_path=$CONFIG_PATH'R2AttUNet.yaml'
 
-python main.py --yaml_path=$CONFIG_PATH'AttU_Net.yaml'
+python main.py --yaml_path=$CONFIG_PATH'R2UNet.yaml'
 
-python main.py --yaml_path=$CONFIG_PATH'R2AttU_Net.yaml'
+python main.py --yaml_path=$CONFIG_PATH'AttUNet.yaml'
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 pip install tqdm
 pip install pyyaml
 pip install einops
+pip install ml_collections
+pip install scipy
